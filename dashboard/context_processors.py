69b72b99f18664bc76f2b095f48bdeed7be8f885
lexiecod3s <shesalexnicole@gmail.com>
2025-11-17 16:21:09 +0800

    when will my misery ends?

--- dashboard/context_processors.py
+++ dashboard/context_processors.py
@@ -1,19 +1,43 @@
-from .models import Flavor
-from django.db.models import F, Q
+from .models import Flavor, Ingredient, Topping, Packaging
+from django.db.models import F
 
 def stock_alerts(request):
-    """
-    Provides global stock alerts for Flavors:
-    - Run Out: quantity = 0
-    - Critical: quantity <= critical_threshold (>0)
-    - Low: quantity <= low_threshold (> critical_threshold)
-    """
-    runout_flavors = Flavor.objects.filter(quantity=0)
+    # ---- FLAVORS ----
     critical_flavors = Flavor.objects.filter(quantity__lte=F('critical_threshold'), quantity__gt=0)
     low_flavors = Flavor.objects.filter(quantity__lte=F('low_threshold'), quantity__gt=F('critical_threshold'))
 
+    # ---- INGREDIENTS ----
+    critical_ingredients = Ingredient.objects.filter(quantity__lte=2, quantity__gt=0)
+    low_ingredients = Ingredient.objects.filter(quantity__lte=5, quantity__gt=2)
+
+    # ---- TOPPINGS ----
+    critical_toppings = Topping.objects.filter(quantity__lte=2, quantity__gt=0)
+    low_toppings = Topping.objects.filter(quantity__lte=5, quantity__gt=2)
+
+    # ---- PACKAGING ----
+    critical_packaging = Packaging.objects.filter(quantity__lte=2, quantity__gt=0)
+    low_packaging = Packaging.objects.filter(quantity__lte=5, quantity__gt=2)
+
+    # Total alert count
+    total_count = (
+        critical_flavors.count() + low_flavors.count() +
+        critical_ingredients.count() + low_ingredients.count() +
+        critical_toppings.count() + low_toppings.count() +
+        critical_packaging.count() + low_packaging.count()
+    )
+
     return {
-        'runout_flavors': runout_flavors,
-        'critical_flavors': critical_flavors,
-        'low_flavors': low_flavors,
+        "critical_flavors": critical_flavors,
+        "low_flavors": low_flavors,
+
+        "critical_ingredients": critical_ingredients,
+        "low_ingredients": low_ingredients,
+
+        "critical_toppings": critical_toppings,
+        "low_toppings": low_toppings,
+
+        "critical_packaging": critical_packaging,
+        "low_packaging": low_packaging,
+
+        "notification_count": total_count,
     }
