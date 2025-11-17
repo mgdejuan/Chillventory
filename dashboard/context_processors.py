51c0dcf2729a35b367cbac78cf32dafae3c7c0a0
mgdejuan <mgdejuan@tip.edu.ph>
2025-11-17 16:11:43 +0800

    ok

---
+++ dashboard/context_processors.py
@@ -0,0 +1,9 @@
+from dashboard.models import Notification
+
+def notification_count(request):
+    if request.user.is_authenticated:
+        count = Notification.objects.filter(is_read=False).count()
+    else:
+        count = 0
+
+    return {'notification_count': count}
